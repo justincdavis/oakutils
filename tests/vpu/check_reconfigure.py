9847fb9d00b8ca17eed7219983476b0bb366bacf
justincdavis <davis.justin@mssm.org>
2023-10-18 17:59:42 -0600

    Incresed timeout to add 5 for each model switch

--- tests/vpu/check_reconfigure.py
+++ tests/vpu/check_reconfigure.py
@@ -24,21 +24,22 @@ def check_method_timout(method: callable, name: str, timeout=5) -> Any:
     return result
 
 def check_network(func: callable):
-    check_method_timout(func, func.__name__, timeout=TIME_TO_RUN + 10)  # add 5 seconds to timeout to account for setup time
+    check_method_timout(func, func.__name__, timeout=TIME_TO_RUN + 15)  # add 5 seconds to timeout for each model
 
-def check_model(model_paths: list[str]) -> None:
+def check_model(model_paths: list[str], timeout: float) -> None:
     if len(dai.Device.getAllAvailableDevices()) == 0:
         return 0  # no device found
     vpu = VPU()
     for model_path in model_paths:
+        print(f"Reconfiguring to {model_path}")
         vpu.reconfigure(model_path)
         t0 = time.perf_counter()
         while True:
-            if time.perf_counter() - t0 > TIME_TO_RUN / len(model_paths):
+            if time.perf_counter() - t0 > timeout:
                 break
             data = np.array(np.random.random((640, 480, 3)) * 255.0, dtype=np.uint8)
             vpu.run(data)
     return 0
 
 def test_vpu():
-    check_network(lambda: check_model([GAUSSIAN_15X15, GAUSSIAN_13X13, GAUSSIAN_11X11]))
+    check_network(lambda: check_model([GAUSSIAN_15X15, GAUSSIAN_13X13, GAUSSIAN_11X11], TIME_TO_RUN / 3))
