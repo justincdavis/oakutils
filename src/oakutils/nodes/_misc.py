c3a6848dadd2de20032f5716d4786dca85de4167
justincdavis <davis.justin@mssm.org>
2023-11-12 22:35:13 -0700

    Added prototype yolo detection network. Will be added into vpu once VPU gets moved to own submodule

---
+++ src/oakutils/nodes/_misc.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+import numpy as np
+
+
+# https://docs.luxonis.com/projects/api/en/latest/samples/Yolo/tiny_yolo/#rgb-tiny-yolo
+def frame_norm(
+    frame: np.ndarray, bbox: tuple[float, float, float, float]
+) -> tuple[int, int, int, int]:
+    """
+    Use to adjust a bounding box returned from a YoloDetectionModel node.
+
+    Parameters
+    ----------
+    frame : np.ndarray
+        The frame to adjust the bounding box for
+    bbox : tuple[float, float, float, float]
+        The bounding box to adjust
+
+
+    Returns
+    -------
+    tuple[int, int, int, int]
+        The adjusted bounding box
+
+    References
+    ----------
+    https://docs.luxonis.com/projects/api/en/latest/samples/Yolo/tiny_yolo/#rgb-tiny-yolo
+    """
+    norm_vals = np.full(len(bbox), frame.shape[0])
+    norm_vals[::2] = frame.shape[1]
+    return (np.clip(np.array(bbox), 0, 1) * norm_vals).astype(int)
