3f1f4c9f8f9a817c0b9469e3e7f314fb27c4491f
justincdavis <davis.justin@mssm.org>
2023-12-03 13:40:35 -0700

    bug fix and example add for legacy cam

--- examples/nodes/models/point_cloud.py
+++ examples/nodes/models/point_cloud.py
@@ -1,11 +1,11 @@
 import depthai as dai
 
 from oakutils.calibration import get_camera_calibration
-from oakutils.nodes import create_stereo_depth, create_xout, get_nn_point_cloud
+from oakutils.nodes import create_stereo_depth, create_xout, get_nn_point_cloud_buffer
 from oakutils.nodes.models.point_cloud import create_point_cloud, create_xyz_matrix
 from oakutils.point_clouds import (
     PointCloudVisualizer,
-    create_point_cloud_from_np,
+    get_point_cloud_from_np_buffer,
     filter_point_cloud,
 )
 
@@ -44,8 +44,8 @@ with dai.Device(pipeline) as device:
 
         counter += 1  # visualizer is super slow
         if counter == 3:
-            np_pcl = get_nn_point_cloud(data)
-            pcl = create_point_cloud_from_np(np_pcl)
+            np_pcl = get_nn_point_cloud_buffer(data)
+            pcl = get_point_cloud_from_np_buffer(np_pcl)
             pcl = filter_point_cloud(pcl)
             pcv.update(pcl)
             counter = 0
