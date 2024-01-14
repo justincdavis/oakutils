4b3dfe7258f2b5b405e6642af77d409f2b7bd838
justincdavis <davis.justin@mssm.org>
2024-01-14 00:57:30 -0700

    remove typings again

--- typings/onnxsim/__init__.pyi
+++
@@ -1,4 +0,0 @@
-from _typeshed import Incomplete
-from onnxsim.onnx_simplifier import main as main, simplify as simplify
-
-x: Incomplete
--- typings/open3d/__init__.pyi
+++
@@ -1,9 +0,0 @@
-from _typeshed import Incomplete
-from open3d.cpu import pybind as pybind
-from open3d.cpu.pybind import camera as camera, core as core, data as data, geometry as geometry, io as io, pipelines as pipelines, t as t, utility as utility
-
-def load_cdll(path): ...
-
-__DEVICE_API__: str
-__version__: str
-shell: Incomplete
