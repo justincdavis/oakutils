994f910f706fd6d29ff2d06ba9cd116dc616900d
justincdavis <davis.justin@mssm.org>
2024-01-25 17:24:28 -0700

    Bump version 1.4.0 -> 1.4.1

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ import oakutils
 project = "oakutils"
 copyright = "2023, Justin Davis"
 author = "Justin Davis"
-version = "1.4.0"
+version = "1.4.1"
 
 assert version == oakutils.__version__  # Make sure version is consistent
 
