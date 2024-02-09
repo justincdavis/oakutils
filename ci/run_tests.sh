c12fe58ee125f321c8288e60d8c9b9f1e9505fd7
justincdavis <davis.justin@mssm.org>
2024-02-08 22:22:38 -0700

    change test calling conventions

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-# Set the path to the tests directory
-test_directory="./tests"
+# # Set the path to the tests directory
+# test_directory="./tests"
 
-# Loop through all Python files in the tests directory
-for file in "$test_directory"/*.py; do
-    echo "Running pytest on $file"
-    python3 -m pytest --log-cli-level=WARNING --full-trace -rP "$file"
-done
+# # Loop through all Python files in the tests directory
+# for file in "$test_directory"/*.py; do
+#     echo "Running pytest on $file"
+#     python3 -m pytest --log-cli-level=WARNING --full-trace -rP "$file"
+# done
+
+python3 -m pytest --log-cli-level=WARNING --full-trace -rP tests/*
