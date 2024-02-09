67b349306562a80d353607e2bb8c904fc277b4f8
justincdavis <davis.justin@mssm.org>
2024-02-09 15:09:22 -0700

    windows testing issues still

--- ci/run_tests.bat
+++ ci/run_tests.bat
@@ -11,8 +11,6 @@ setlocal enabledelayedexpansion
 @REM )
 
 @REM python3 -m pytest --log-cli-level=WARNING --full-trace -rP .\tests\*
-python3 -m pytest --log-cli-level=WARNING --full-trace -rP .\tests\check_models_install.py
-python3 -m pytest --log-cli-level=WARNING --full-trace -rP .\tests\check_models_run.py
-python3 -m pytest --log-cli-level=WARNING --full-trace -rP .\tests\check_vpu.py
+python3 -m pytest --log-cli-level=WARNING --full-trace -rP tests || exit /b 1
 
 endlocal
