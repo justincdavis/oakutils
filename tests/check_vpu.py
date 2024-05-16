d8ef72769ba970a6a89c6f2da7aeaac309b746b9
justincdavis <davis.justin@mssm.org>
2024-05-16 11:59:39 -0600

    Begin adding tests for multi netowrk VPU

--- tests/check_vpu.py
+++ tests/check_vpu.py
@@ -1,6 +1,7 @@
-from .vpu import check_reconfigure, check_single_model
+from .vpu import check_reconfigure, check_single_model, check_multi_network
 
 
 def test_vpu():
     check_reconfigure.test_vpu()
     check_single_model.test_vpu()
+    check_multi_network.test_vpu()
